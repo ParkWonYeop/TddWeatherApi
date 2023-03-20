0e80d723bcc61dc7fbd72bf918040957ce938e24
ParkWonYeop <wyp9679@gmail.com>
2023-03-20 17:42:15 +0900

    add file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WeatherModule } from './weather/weather.module';
-import ormconfig from './ormConfig/ormconfig';
 import { LocalModule } from './local/local.module';
 import { ValidationPipe } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
+import { ConfigurationModule } from './configuration.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/entities/**/*.js'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
     WeatherModule,
     LocalModule,
+    ConfigurationModule,
   ],
   controllers: [AppController],
   providers: [
